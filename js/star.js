f8c770ccf350965336e1f9ec80f54623bb976024
Mark Spina <lehon.lachance@gmail.com>
2016-11-08 19:25:00 -0800

    Elections aren't fun

--- js/star.js
+++ js/star.js
@@ -3,21 +3,42 @@ var starImage;
 
 var starChoices = ['star_red', 'star_yellow', 'star_green', 'star_blue'];
 
+// movement variables
+var movingStar = true;
+var xVelocity;
+var yVelocity;
+
 function Star(currentGame, layer) {
 	game = currentGame;
 	
-	starImage = layer.create(200, 200, starChoices[game.rnd.integerInRange(0, starChoices.length)]);
+	var starColor = starChoices[game.rnd.integerInRange(0, starChoices.length)];
+	console.log('Test: ' + starColor);
+	starImage = layer.create(200, 200, starColor);
 	starImage.anchor.setTo(.5, .5);
 	
 	var starScale = game.rnd.integerInRange(50, 100) / 100;
 	starImage.scale.setTo(starScale, starScale);
 	starImage.x = game.rnd.integerInRange(0, 500);
 	starImage.y = 100;
-	console.log('Test Hit');
+	
+	if(movingStar) {
+		game.time.events.add(0, starMovement, this);
+	}
+	
+	game.time.events.add(Phaser.Timer.SECOND * 3, removeStar, this);
 }
 
 Star.prototype.generateNewStar = function () {
 	 
+}
+
+function starMovement() {
+	
+}
+
+function removeStar() {
+	console.log('Poof');
+	this.starImage.visible = false;
 }
 
 /*
--- js/star_manager.js
+++ js/star_manager.js
@@ -13,8 +13,18 @@ function StarManager(currentGame, starLayer) {
 }
 
 function generateStars(starLayer) {
+	var newStar;
+	
 	for(var i = 0; i < starAmount; i++) {
-		var newStar = new Star(game, starLayer);
+		newStar = new Star(game, starLayer);
+		newStar.generateNewStar();
+		
+		var removeStar = function() {
+			console.log('Poof');
+			newStar.starImage.visible = false;
+		}
+		
+		game.time.events.add(Phaser.Timer.SECOND * 1, removeStar, newStar);
 		stars.push(newStar);
 	}
 }
